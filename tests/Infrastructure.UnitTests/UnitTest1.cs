9c1e629380cd28d9e4118ca0633f2a29683b99e1
Salvador Nicolas <snicoper@gmail.com>
2024-06-30 12:39:31 +0200

    Creados proyectos para tests

---
+++ tests/Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace Infrastructure.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ tests/WebApi.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace WebApi.IntegrationTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
