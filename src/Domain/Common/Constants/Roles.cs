b63492f60cc120c930b3c1689c15e836046b6482
Salvador Nicolas <snicoper@gmail.com>
2024-07-06 14:53:44 +0200

    Eliminado Role Anonymous

--- src/Domain/Common/Constants/Roles.cs
+++ src/Domain/Common/Constants/Roles.cs
@@ -5,6 +5,4 @@ public abstract class Roles
     public const string Admin = nameof(Admin);
 
     public const string Staff = nameof(Staff);
-
-    public const string Anonymous = nameof(Anonymous);
 }
