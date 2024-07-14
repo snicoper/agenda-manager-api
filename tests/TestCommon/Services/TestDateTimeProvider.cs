55668b3d05cfe1ee33d7d3c11fcd6160d105d84f
Salvador Nicolas <snicoper@gmail.com>
2024-07-15 00:38:23 +0200

    Implementado TestDateTimeProvider

---
+++ tests/TestCommon/Services/TestDateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿using AgendaManager.Application.Common.Interfaces.Clock;
+
+namespace AgendaManager.TestCommon.Services;
+
+public class TestDateTimeProvider(DateTimeOffset? fixedDateTimeOffset = null)
+    : IDateTimeProvider
+{
+    public DateTimeOffset UtcNow => fixedDateTimeOffset ?? DateTimeOffset.UtcNow;
+}
