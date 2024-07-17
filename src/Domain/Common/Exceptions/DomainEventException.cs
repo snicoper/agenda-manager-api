d85c99a65fffb6607333dd14389fb62304efd87f
Salvador Nicolas <snicoper@gmail.com>
2024-07-17 22:41:02 +0200

    Creado DomainEventException

---
+++ src/Domain/Common/Exceptions/DomainEventException.cs
@@ -0,0 +1,23 @@
+﻿namespace AgendaManager.Domain.Common.Exceptions;
+
+public class DomainEventException : Exception
+{
+    public DomainEventException()
+    {
+    }
+
+    public DomainEventException(string message)
+        : base(message)
+    {
+    }
+
+    public DomainEventException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+
+    public DomainEventException(string eventName, object key)
+        : base($"Exception in \"{eventName}\" ({key}) .")
+    {
+    }
+}
