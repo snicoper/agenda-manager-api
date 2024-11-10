12af0bfc2f4ef7c3ee5e748b2447bdc049458b5d
Salvador Nicolas <snicoper@gmail.com>
2024-11-10 13:39:03 +0100

    Implementado PasswordHashTests

--- tests/Domain.UnitTests/Common/ValueObjects/EmailAddress/EmailAddressTests.cs
+++ tests/Domain.UnitTests/Common/ValueObjects/EmailAddress/EmailAddressTests.cs
@@ -1,18 +1,18 @@
 ﻿using AgendaManager.Domain.Common.ValueObjects.EmailAddress;
 using FluentAssertions;
 
-namespace AgendaManager.Domain.UnitTests.Users.ValueObjects;
+namespace AgendaManager.Domain.UnitTests.Common.ValueObjects.EmailAddress;
 
-public class EmailTests
+public class EmailAddressTests
 {
     [Fact]
-    public void Email_ShouldCreateNewEmail_WhenValidEmailIsPassed()
+    public void EmailAddress_ShouldCreateNewEmail_WhenValidEmailIsProvided()
     {
         // Arrange
         const string email = "test@test.com";
 
         // Act
-        var emailValueObject = EmailAddress.From(email);
+        var emailValueObject = Domain.Common.ValueObjects.EmailAddress.EmailAddress.From(email);
 
         // Assert
         emailValueObject.Value.Should().Be(email);
@@ -24,24 +24,24 @@ public class EmailTests
     [InlineData("test")]
     [InlineData("test@")]
     [InlineData("test@123123")]
-    public void Email_ShouldThrowInvalidEmailException_WhenInvalidEmailIsPassed(string email)
+    public void EmailAddress_ShouldThrowInvalidEmailException_WhenInvalidEmailIsProvided(string email)
     {
         // Act
-        var act = () => EmailAddress.From(email);
+        var act = () => Domain.Common.ValueObjects.EmailAddress.EmailAddress.From(email);
 
         // Assert
         act.Should().Throw<InvalidEmailAddressException>();
     }
 
     [Fact]
-    public void Email_ShouldThrowInvalidEmailException_WhenEmailLengthIsGreaterThan256()
+    public void EmailAddress_ShouldThrowInvalidEmailException_WhenEmailLengthIsGreaterThan256()
     {
         // Arrange
         var emailName = new string('a', 248);
         var email = $"{emailName}@test.com";
 
         // Act
-        var act = () => EmailAddress.From(email);
+        var act = () => Domain.Common.ValueObjects.EmailAddress.EmailAddress.From(email);
 
         // Assert
         act.Should().Throw<InvalidEmailAddressException>();
---
+++ tests/Domain.UnitTests/Users/ValueObjects/PasswordHashTests.cs
@@ -0,0 +1,98 @@
+﻿using AgendaManager.Domain.Common.Responses;
+using AgendaManager.Domain.Users.Exceptions;
+using AgendaManager.Domain.Users.Interfaces;
+using AgendaManager.Domain.Users.ValueObjects;
+using FluentAssertions;
+using NSubstitute;
+
+namespace AgendaManager.Domain.UnitTests.Users.ValueObjects;
+
+public class PasswordHashTests
+{
+    private readonly IPasswordHasher _passwordHasher = Substitute.For<IPasswordHasher>();
+    private readonly IPasswordPolicy _passwordPolicy = Substitute.For<IPasswordPolicy>();
+
+    [Fact]
+    public void PasswordHash_ShouldReturnCorrectHash_WhenValidPasswordIsProvidedInFromHashed()
+    {
+        // Arrange
+        const string hashedPassword = "validPassword";
+
+        // Act
+        var passwordHash = PasswordHash.FromHashed(hashedPassword);
+
+        // Assert
+        passwordHash.HashedValue.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void PasswordHash_ShouldReturnThrowException_WhenInvalidPasswordIsProvidedInFromHashed(string hashedPassword)
+    {
+        // Assert
+        Assert.Throws<UserDomainException>(() => PasswordHash.FromHashed(hashedPassword));
+    }
+
+    [Fact]
+    public void PasswordHash_ShouldReturnResultSuccess_WhenValidPasswordIsProvidedInFromRaw()
+    {
+        // Arrange
+        const string rawPassword = "validPassword";
+        _passwordPolicy.ValidatePassword(Arg.Any<string>()).Returns(Result.Success());
+        _passwordHasher.HashPassword(Arg.Any<string>()).Returns(rawPassword);
+
+        // Act
+        var passwordHashResult = PasswordHash.FromRaw(rawPassword, _passwordHasher, _passwordPolicy);
+
+        // Assert
+        passwordHashResult.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public void PasswordHash_ShouldReturnResultFailure_WhenInValidPasswordIsProvidedInFromRaw()
+    {
+        // Arrange
+        const string rawPassword = "validPassword";
+        _passwordPolicy.ValidatePassword(Arg.Any<string>()).Returns(Result.Failure());
+
+        // Act
+        var passwordHashResult = PasswordHash.FromRaw(rawPassword, _passwordHasher, _passwordPolicy);
+
+        // Assert
+        passwordHashResult.IsFailure.Should().BeTrue();
+    }
+
+    [Fact]
+    public void PasswordHash_ShouldReturnTrue_WhenVerifyIsSuccessful()
+    {
+        // Arrange
+        const string rawPassword = "validPassword";
+        _passwordPolicy.ValidatePassword(Arg.Any<string>()).Returns(Result.Success());
+        _passwordHasher.VerifyPassword(Arg.Any<string>(), Arg.Any<PasswordHash>()).Returns(true);
+
+        // Act
+        var passwordHash = PasswordHash.FromRaw(rawPassword, _passwordHasher, _passwordPolicy);
+        var verifyResult = passwordHash.Value?.Verify(rawPassword, _passwordHasher);
+
+        // Assert
+        verifyResult.Should().BeTrue();
+    }
+
+    [Fact]
+    public void PasswordHash_ShouldReturnFalse_WhenVerifyIsUnSuccessful()
+    {
+        // Arrange
+        const string rawPassword = "validPassword";
+        const string rawPasswordVerify = "validPassword2";
+        _passwordPolicy.ValidatePassword(Arg.Any<string>()).Returns(Result.Success());
+        _passwordHasher.VerifyPassword(Arg.Any<string>(), Arg.Any<PasswordHash>()).Returns(false);
+
+        // Act
+        var passwordHash = PasswordHash.FromRaw(rawPassword, _passwordHasher, _passwordPolicy);
+        var verifyResult = passwordHash.Value?.Verify(rawPasswordVerify, _passwordHasher);
+
+        // Assert
+        verifyResult.Should().BeFalse();
+    }
+}
