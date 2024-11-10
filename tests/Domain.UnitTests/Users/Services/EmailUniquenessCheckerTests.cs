ea202bf720a4c7931f2b1dc780136fab8ebea88b
Salvador Nicolas <snicoper@gmail.com>
2024-11-10 14:03:40 +0100

    Implements EmailUniquenessCheckerTests

---
+++ tests/Domain.UnitTests/Users/Services/EmailUniquenessCheckerTests.cs
@@ -0,0 +1,46 @@
+﻿using AgendaManager.Domain.Common.ValueObjects.EmailAddress;
+using AgendaManager.Domain.Users.Interfaces;
+using AgendaManager.Domain.Users.Services;
+using FluentAssertions;
+using NSubstitute;
+
+namespace AgendaManager.Domain.UnitTests.Users.Services;
+
+public class EmailUniquenessCheckerTests
+{
+    private readonly IUserRepository _userRepository = Substitute.For<IUserRepository>();
+    private readonly EmailUniquenessChecker _sut;
+
+    public EmailUniquenessCheckerTests()
+    {
+        _sut = new EmailUniquenessChecker(_userRepository);
+    }
+
+    [Fact]
+    public async Task EmailUniquenessChecker_IsUnique_ShouldReturnSuccess_WhenEmailIsNotInUse()
+    {
+        // Arrange
+        var email = EmailAddress.From("email@email.com");
+        _userRepository.EmailExistsAsync(Arg.Any<EmailAddress>(), Arg.Any<CancellationToken>()).Returns(false);
+
+        // Act
+        var result = await _sut.IsUnique(email);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task EmailUniquenessChecker_IsUnique_ShouldReturnFailure_WhenEmailIsInUse()
+    {
+        // Arrange
+        var email = EmailAddress.From("email@email.com");
+        _userRepository.EmailExistsAsync(Arg.Any<EmailAddress>(), Arg.Any<CancellationToken>()).Returns(true);
+
+        // Act
+        var result = await _sut.IsUnique(email);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+}
