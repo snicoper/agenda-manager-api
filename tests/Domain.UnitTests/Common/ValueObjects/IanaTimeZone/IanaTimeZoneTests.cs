6252d558047ffb62a9d85ea933a3fd874a9392f3
Salvador Nicolas <snicoper@gmail.com>
2024-11-12 19:24:57 +0100

    Implementado IanaTimeZoneTests

--- tests/Domain.UnitTests/Common/ValueObjects/IanaTimeZone/IanaTimeZoneTests.cs
+++ tests/Domain.UnitTests/Common/ValueObjects/IanaTimeZone/IanaTimeZoneTests.cs
@@ -6,24 +6,121 @@ namespace AgendaManager.Domain.UnitTests.Common.ValueObjects.IanaTimeZone;
 
 public class IanaTimeZoneTests
 {
-    [Fact]
-    public void IanaTimeZone_Should_BeValid()
+    [Theory]
+    [InlineData(IanaTimeZoneConstants.UTC)]
+    [InlineData(IanaTimeZoneConstants.AsiaTokyo)]
+    [InlineData(IanaTimeZoneConstants.EuropeMadrid)]
+    [InlineData(IanaTimeZoneConstants.AmericaNewYork)]
+    public void IanaTimeZone_FromIana_ShouldCreate_WhenValidTimeZone(string validIanaId)
     {
-        // Arrange
-        var ianaTimeZone = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(IanaTimeZoneConstants.EuropeMadrid);
-
         // Act
-        ianaTimeZone.Value.Should().Be(IanaTimeZoneConstants.EuropeMadrid);
+        var timeZone = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(validIanaId);
+
+        // Assert
+        timeZone.Value.Should().Be(validIanaId);
+        timeZone.Info.Should().NotBeNull();
     }
 
     [Fact]
-    public void IanaTimeZone_ShouldRaiseException_BeInValid()
+    public void IanaTimeZone_FromIana_ShouldThrowException_WhenValueIsNull()
+    {
+        // Act
+        var action = () => Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(null!);
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData(IanaTimeZoneConstants.InvalidTimeZone)]
+    public void IanaTimeZone_FromIana_ShouldThrowException_WhenValueIsInvalid(string invalidTimeZone)
+    {
+        // Act
+        var action = () => Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(invalidTimeZone);
+
+        // Assert
+        action.Should().Throw<CalendarSettingsException>();
+    }
+
+    [Fact]
+    public void IanaTimeZone_FromTimeZoneInfo_ShouldThrowException_WhenValueIsNull()
+    {
+        // Act
+        var action = () => Domain.Calendars.ValueObjects.IanaTimeZone.FromTimeZoneInfo(null!);
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void IanaTimeZone_FromTimeZoneInfo_ShouldCreateValidTimeZone_FromSystemTimeZone()
     {
         // Arrange
-        var ianaTimeZone = () => Domain.Calendars.ValueObjects.IanaTimeZone.FromIana("invalid");
+        var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(
+            OperatingSystem.IsWindows() ? IanaTimeZoneConstants.ValidWindows : IanaTimeZoneConstants.EuropeMadrid);
 
         // Act
-        ianaTimeZone.Should().Throw<CalendarSettingsException>()
-            .WithMessage("Invalid IANA time zone ID.");
+        var timeZone = Domain.Calendars.ValueObjects.IanaTimeZone.FromTimeZoneInfo(timeZoneInfo);
+
+        // Assert
+        timeZone.Should().NotBeNull();
+        timeZone.Info.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void IanaTimeZone_ShouldBeEqual_WhenEqualTimeZones()
+    {
+        // Arrange
+        var timeZone1 = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(IanaTimeZoneConstants.EuropeMadrid);
+        var timeZone2 = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana(IanaTimeZoneConstants.EuropeMadrid);
+
+        // Assert
+        timeZone1.Should().Be(timeZone2);
+        timeZone1.GetHashCode().Should().Be(timeZone2.GetHashCode());
+    }
+
+    [Fact]
+    public void IanaTimeZone_ShouldNotBeEqual_WhenDifferentTimeZones()
+    {
+        // Arrange
+        var timeZone1 = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana("Europe/Madrid");
+        var timeZone2 = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana("Europe/London");
+
+        // Assert
+        timeZone1.Should().NotBe(timeZone2);
+        timeZone1.GetHashCode().Should().NotBe(timeZone2.GetHashCode());
+    }
+
+    [Fact]
+    public void TimeZone_Should_Handle_UTC_Correctly()
+    {
+        // Arrange & Act
+        var timeZone = Domain.Calendars.ValueObjects.IanaTimeZone.FromIana("UTC");
+
+        // Assert
+        timeZone.Value.Should().Be("UTC");
+        timeZone.Info.BaseUtcOffset.Should().Be(TimeSpan.Zero);
+    }
+
+    [Fact]
+    public void FromTimeZoneInfo_Should_ConvertWindows_To_Iana_Correctly()
+    {
+        if (!OperatingSystem.IsWindows())
+        {
+            return;
+        }
+
+        // Arrange
+        var windowsTimeZone = TimeZoneInfo.FindSystemTimeZoneById(IanaTimeZoneConstants.ValidWindows);
+
+        // Act
+        var timeZone = Domain.Calendars.ValueObjects.IanaTimeZone.FromTimeZoneInfo(windowsTimeZone);
+
+        // Assert
+        timeZone.Should().NotBeNull();
+        timeZone.Value.Should().NotBe(IanaTimeZoneConstants.ValidWindows);
+        timeZone.Info.Should().NotBeNull();
     }
 }
--- tests/TestCommon/Constants/IanaTimeZoneConstants.cs
+++ tests/TestCommon/Constants/IanaTimeZoneConstants.cs
@@ -2,7 +2,11 @@
 
 public static class IanaTimeZoneConstants
 {
+    public const string ValidWindows = "Romance Standard Time";
+    public const string InvalidTimeZone = "Invalid/TimeZone";
+
+    public const string UTC = "UTC";
     public const string EuropeMadrid = "Europe/Madrid";
     public const string AmericaNewYork = "America/New_York";
-    public const string UTC = "UTC";
+    public const string AsiaTokyo = "Asia/Tokyo";
 }
