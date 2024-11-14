a609032e1a41031ba9eb46972398beb0454ee44d
Salvador Nicolas <snicoper@gmail.com>
2024-11-14 16:08:04 +0100

    Implementado ResourceCreateTests

---
+++ tests/Domain.UnitTests/Resources/ResourceAggregate/ResourceCreateTests.cs
@@ -0,0 +1,75 @@
+﻿using AgendaManager.Domain.Resources.Exceptions;
+using AgendaManager.TestCommon.Factories;
+using FluentAssertions;
+
+namespace AgendaManager.Domain.UnitTests.Resources.ResourceAggregate;
+
+public class ResourceCreateTests
+{
+    [Fact]
+    public void Create_ShouldReturnResource_ValidResource()
+    {
+        // Arrange
+        var resource = ResourceFactory.CreateResource();
+
+        // Assert
+        resource.Should().NotBeNull();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Create_ShouldThrowException_WhenInvalidNameProvided(string invalidName)
+    {
+        var action = () => ResourceFactory.CreateResource(name: invalidName);
+
+        // Assert
+        action.Should().Throw<ResourceDomainException>()
+            .WithMessage("Resource name must be between 1 and 50 characters long.");
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(257)]
+    public void Create_ShouldThrowException_WhenInvalidNameLengthAreProvided(int nameLength)
+    {
+        // Arrange
+        var invalidName = new string('*', nameLength);
+
+        // Act
+        var action = () => ResourceFactory.CreateResource(name: invalidName);
+
+        // Assert
+        action.Should().Throw<ResourceDomainException>()
+            .WithMessage("Resource name must be between 1 and 50 characters long.");
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Create_ShouldThrowException_WhenInvalidDescriptionProvided(string invalidDescription)
+    {
+        // Act
+        var action = () => ResourceFactory.CreateResource(description: invalidDescription);
+
+        // Assert
+        action.Should().Throw<ResourceDomainException>()
+            .WithMessage("Resource description must be between 1 and 500 characters long.");
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(501)]
+    public void Create_ShouldThrowException_WhenInvalidDescriptionLengthAreProvided(int descriptionLength)
+    {
+        // Arrange
+        var invalidDescription = new string('*', descriptionLength);
+
+        // Act
+        var action = () => ResourceFactory.CreateResource(description: invalidDescription);
+
+        // Assert
+        action.Should().Throw<ResourceDomainException>()
+            .WithMessage("Resource description must be between 1 and 500 characters long.");
+    }
+}
---
+++ tests/TestCommon/Factories/ResourceFactory.cs
@@ -0,0 +1,34 @@
+﻿using AgendaManager.Domain.Calendars.ValueObjects;
+using AgendaManager.Domain.Common.ValueObjects.ColorScheme;
+using AgendaManager.Domain.Resources;
+using AgendaManager.Domain.Resources.ValueObjects;
+using AgendaManager.Domain.ResourceTypes.ValueObjects;
+using AgendaManager.Domain.Users.ValueObjects;
+
+namespace AgendaManager.TestCommon.Factories;
+
+public static class ResourceFactory
+{
+    public static Resource CreateResource(
+        ResourceId? resourceId = null,
+        UserId? userId = null,
+        CalendarId? calendarId = null,
+        ResourceTypeId? resourceTypeId = null,
+        string? name = null,
+        string? description = null,
+        ColorScheme? colorScheme = null,
+        bool? isActive = null)
+    {
+        var resource = Resource.Create(
+            id: resourceId ?? ResourceId.Create(),
+            userId: userId ?? UserId.Create(),
+            calendarId: calendarId ?? CalendarId.Create(),
+            typeId: resourceTypeId ?? ResourceTypeId.Create(),
+            name: name ?? "Default Name",
+            description: description ?? "Default Description",
+            colorScheme: colorScheme ?? ColorScheme.From("#ffffff", "#000000"),
+            isActive: isActive ?? true);
+
+        return resource;
+    }
+}
