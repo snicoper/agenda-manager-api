614f6d5995d77f967450e1e9a9e3cea4f60d0cf1
Salvador Nicolas <snicoper@gmail.com>
2024-11-21 03:49:00 +0100

    Creado UserRoleTests

---
+++ tests/Domain.UnitTests/Users/Entities/UserRoles/UserRoleTests.cs
@@ -0,0 +1,19 @@
+﻿using AgendaManager.TestCommon.Factories;
+using FluentAssertions;
+
+namespace AgendaManager.Domain.UnitTests.Users.Entities.UserRoles;
+
+public class UserRoleTests
+{
+    [Fact]
+    public void UserRole_ShouldCreateUserRole()
+    {
+        // Act
+        var userRole = UserRoleFactory.CreateUserRole();
+
+        // Assert
+        userRole.Should().NotBeNull();
+        userRole.UserId.Should().Be(userRole.UserId);
+        userRole.RoleId.Should().Be(userRole.RoleId);
+    }
+}
