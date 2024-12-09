3a2916e66b135286ea8718923aebb94c72df42ab
Salvador Nicolas <snicoper@gmail.com>
2024-12-09 06:12:03 +0100

    Creado PasswordGenerator

---
+++ src/Application/Users/EventHandlers/UserCreatedDomainEventHandler.cs
@@ -0,0 +1,14 @@
+﻿using AgendaManager.Application.Common.Abstractions;
+using AgendaManager.Domain.Users.Events;
+using Microsoft.Extensions.Logging;
+
+namespace AgendaManager.Application.Users.EventHandlers;
+
+public class UserCreatedDomainEventHandler(ILogger<UserCreatedDomainEventHandler> logger)
+    : BaseEventHandler<UserCreatedDomainEvent>(logger)
+{
+    protected override Task HandleEvent(UserCreatedDomainEvent notification, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Domain/Common/Utils/PasswordGenerator.cs
@@ -0,0 +1,38 @@
+﻿namespace AgendaManager.Domain.Common.Utils;
+
+public static class PasswordGenerator
+{
+    private const string Uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private const string Lowercase = "abcdefghijklmnopqrstuvwxyz";
+    private const string Numbers = "0123456789";
+    private const string Specials = "#?!@$%^&*-";
+
+    public static string GeneratePassword(int length = 12)
+    {
+        if (length < 8)
+        {
+            throw new ArgumentException("Password length must be at least 8 characters");
+        }
+
+        var random = new Random();
+        var password = new char[length];
+
+        // Garantizar al menos uno de cada tipo.
+        password[0] = Uppercase[random.Next(Uppercase.Length)];
+        password[1] = Lowercase[random.Next(Lowercase.Length)];
+        password[2] = Numbers[random.Next(Numbers.Length)];
+        password[3] = Specials[random.Next(Specials.Length)];
+
+        // Todos los caracteres permitidos para el resto.
+        const string allChars = Uppercase + Lowercase + Numbers + Specials;
+
+        // Rellenar el resto de posiciones.
+        for (var i = 4; i < length; i++)
+        {
+            password[i] = allChars[random.Next(allChars.Length)];
+        }
+
+        // Mezclar todo para que no siempre siga el mismo patrón.
+        return new string(password.OrderBy(_ => random.Next()).ToArray());
+    }
+}
