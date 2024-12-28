9cf4fc95ca7e19845e227bd0b752157569825498
Salvador Nicolas <snicoper@gmail.com>
2024-12-28 06:58:06 +0100

    Eliminado None y renombrado Facility a Place

--- src/Domain/Resources/Enums/ResourceCategory.cs
+++ src/Domain/Resources/Enums/ResourceCategory.cs
@@ -4,14 +4,11 @@ namespace AgendaManager.Domain.Resources.Enums;
 
 public enum ResourceCategory
 {
-    [Display(Name = "None")]
-    None = 0,
-
     [Display(Name = "Staff")]
     Staff = 1,
 
-    [Display(Name = "Facility")]
-    Facility = 2,
+    [Display(Name = "Place")]
+    Place = 2,
 
     [Display(Name = "Equipment")]
     Equipment = 3
